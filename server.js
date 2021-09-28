92893939da8421b669f599a08bdd9d83326afae2
Charlie_mac <tchoi.tab@gmail.com>
2021-09-28 13:43:28 +0900

    Challenge completed

--- server.js
+++ server.js
@@ -24,6 +24,48 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+const mWareLogger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.path} - ${req.ip}`
+  );
+  next();
+};
+app.use( mWareLogger );
+
+const dateToJson = (req, res) => {
+  const is_req_tickcount =
+    parseFloat(req.params.datetick).toString().length ===
+    req.params.datetick.length;
+
+  let tickcount;
+  let datestring;
+  if( is_req_tickcount ) {
+    tickcount = req.params.datetick;
+    datestring = new Date(parseFloat(req.params.datetick));
+  } else {
+    datestring = new Date(req.params.datetick);
+    tickcount = datestring.getTime();
+  }
+
+  if( !tickcount || !datestring || parseFloat(tickcount) === NaN || datestring.toUTCString() === 'Invalid Date' ) {
+    res.json( { error : "Invalid Date" } );
+    return ;
+  }
+
+  const response = {
+    unix: parseFloat(tickcount),
+    utc: datestring.toUTCString()
+  };
+  res.json( response );
+}
+
+const parseShit = (req, res, next) => {
+  req.params.datetick = new Date();
+  next();
+};
+ 
+app.get("/api", parseShit, dateToJson );
+app.get("/api/:datetick", dateToJson );
 
 
 // listen for requests :)
